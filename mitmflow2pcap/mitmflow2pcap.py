5206c6d869955b9aa218a1392c1279a904b73673
Anders Einar Hilden <hildenae@gmail.com>
2017-01-22 06:36:14 +0100

    Drop most printing

--- mitmflow2pcap/mitmflow2pcap.py
+++ mitmflow2pcap/mitmflow2pcap.py
@@ -41,42 +41,41 @@ with open(sys.argv[1], "rb") as logfile:
         pkts = []
         cseq=1
         sseq=1000
-        #ci, cp, si, sp = ("0.0.0.0","0","0.0.0.0","0")
         client_ports = []
         for f in freader.stream():
             
             server_ip, server_port = f.server_conn.ip_address.address
             client_ip, client_port = f.server_conn.source_address.address
             if server_port == 443:
-                # We live about the server port to get rid of a Wireshark warning
+                # We lie about the server port to get rid of a Wireshark warning
                 server_port = 80;
 
             if client_port in client_ports:
-                # If the client port is reused, we live about it to get rid of a 
+                # If the client port is reused, we lie about it to get rid of a 
                 # Wireshark warning
                 client_port = client_ports[-1] + 1;
                 client_ports.append(client_port)
             else:
                 client_ports.append(client_port)
-            print("REQUEST FLOW: %s:%s => %s:%s" % (client_ip, client_port, server_ip, server_port))
-            print("-"*100)
-            print("\033[92m")
+            #print("REQUEST FLOW: %s:%s => %s:%s" % (client_ip, client_port, server_ip, server_port))
+            #print("-"*100)
+            #print("\033[92m")
             #request = f.request.method path 
             r=f.request
             REQ="%s %s %s\r\n" % (r.method, r.path, r.http_version)
             for h in r.headers:
                 REQ=REQ+"%s: %s\r\n" % (h, f.request.headers[h])
-            print(REQ)
-            print("")
+            #print(REQ)
+            #print("")
             REQ=REQ+"\r\n"
             
             bp = bytearray()
             bp.extend(map(ord, REQ))
             bp.extend(r.raw_content)
             
-            sys.stdout.flush()
-            sys.stdout.buffer.write(r.raw_content)
-            sys.stdout.flush()
+            #sys.stdout.flush()
+            #sys.stdout.buffer.write(r.raw_content)
+            #sys.stdout.flush()
        
             # Hankshake 
             #SYN (CS)
@@ -92,21 +91,21 @@ with open(sys.argv[1], "rb") as logfile:
             req = EtherIP(client_ip, server_ip)/TCP(sport=client_port,dport=server_port,flags='PA',ack=sseq,seq=cseq)/bytes(bp)
             #pkts.append(EtherIP(client_ip, server_ip)/TCP(sport=client_port,dport=server_port,flags='PA',ack=sseq,seq=cseq)/bytes(b))
             pkts.append(req)
-            print(pkts)
-            print("\033[00m")
-            print("-"*100)
+            #print(pkts)
+            #print("\033[00m")
+            #print("-"*100)
             
-            print("RESPONSE FLOW: %s:%s => %s:%s" % (server_ip, server_port, client_ip, client_port))
-            print("-"*100)
-            print("\033[91m")
+            #print("RESPONSE FLOW: %s:%s => %s:%s" % (server_ip, server_port, client_ip, client_port))
+            #print("-"*100)
+            #print("\033[91m")
             q=f.response
-            print("< %s %s %s" % (q.http_version, q.status_code, q.reason))
+            #print("< %s %s %s" % (q.http_version, q.status_code, q.reason))
             RESP="%s %s %s\r\n" % (q.http_version, q.status_code, q.reason)
             for h in q.headers:
-                print("< %s: %s" % (h, q.headers[h]))
+                #print("< %s: %s" % (h, q.headers[h]))
                 RESP=RESP+"%s: %s\r\n" % (h, q.headers[h])
             RESP=RESP+"\r\n"
-            print("")
+            #print("")
 
             bq = bytearray()
             bq.extend(map(ord, RESP))
@@ -132,14 +131,14 @@ with open(sys.argv[1], "rb") as logfile:
 
             sseq=sseq+100; cseq=cseq+100;
 
-            sys.stdout.flush()
-            sys.stdout.buffer.write(q.content)
-            sys.stdout.flush()
-            print("\033[00m")
-            print("")
-            print("-"*100)
+            #sys.stdout.flush()
+            #sys.stdout.buffer.write(q.content)
+            #sys.stdout.flush()
+            #print("\033[00m")
+            #print("")
+            #print("-"*100)
             #pp.pprint(f.response.get_state())
-            print("")
-        wrpcap("temp.pcap",pkts,linktype=1)
+            #print("")
+        wrpcap(sys.argv[2],pkts,linktype=1)
     except FlowReadException as e:
         print("Flow file corrupted: {}".format(e))
